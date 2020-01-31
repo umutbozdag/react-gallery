d363e8c682515f8266c1ec96ed1cc310f7ace4f5
Umut <bozdag321@hotmail.com>
2020-01-31 13:04:59 +0300

    Add eslint, Spinner

---
+++ src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Spinner() {
+    return (
+        <div>
+            <div className="spinner-border" role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+        </div>
+    )
+}

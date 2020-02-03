3d5518a3c681492974d07e6e53f96274ff562ad0
Umut <bozdag321@hotmail.com>
2020-02-03 23:09:28 +0300

    Add line and pie chart for photo details

--- src/components/PhotoStatistics/PhotoStatistics.js
+++ src/components/PhotoStatistics/PhotoStatistics.js
@@ -1,5 +1,7 @@
-import React, { useEffect, useState, Component } from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types';
+import { Line, Pie } from 'react-chartjs-2';
+import './PhotoStatistics.css';
 
 export default class PhotoStatistics extends Component {
 
@@ -7,7 +9,12 @@ export default class PhotoStatistics extends Component {
         super(props);
 
         this.state = {
-            photoStatistics: {}
+            photoStatistics: {},
+            chartInfo: {},
+            dates: [],
+            views: [],
+            likes: [],
+            downloads: []
         }
     }
 
@@ -15,22 +22,193 @@ export default class PhotoStatistics extends Component {
         this.getPhotoStatistics();
     }
 
+
+
     getPhotoStatistics = () => {
         const { photoId } = this.props;
 
         fetch(`https://api.unsplash.com/photos/${photoId}/statistics?client_id=${process.env.REACT_APP_API_KEY}`)
             .then(res => res.json())
             .then(data => {
-                this.setState({ data });
+                this.setState({ photoStatistics: data });
                 console.log(data);
+
+                const dates = [];
+                const views = [];
+                const likes = [];
+                const downloads = [];
+                this.state.photoStatistics.views.historical.values.forEach(element => {
+                    const info = element;
+                    dates.push(info.date);
+
+                    views.push(info.value);
+
+                    this.setState({ dates: dates, views: views });
+
+                });
+
+                this.state.photoStatistics.likes.historical.values.forEach(element => {
+                    const info = element;
+
+                    likes.push(info.value);
+
+                    this.setState({ likes: likes });
+                });
+
+                this.state.photoStatistics.downloads.historical.values.forEach(element => {
+                    const info = element;
+
+                    downloads.push(info.value);
+
+                    this.setState({ downloads: downloads });
+                });
+
+                console.log(this.state.dates);
+                console.log(this.state.views);
+                console.log(this.state.likes);
+                console.log(this.state.downloads);
+
             })
             .catch(err => console.log(err));
     }
 
+    datasetKeyProvider = () => {
+        return Math.random();
+    }
+
     render() {
+
+        const { photoStatistics, views, likes, downloads } = this.state;
+
+        const lineChartData = {
+            labels: [...this.state.dates],
+            datasets: [
+                {
+                    label: 'Views',
+                    fill: false,
+                    lineTension: 1,
+                    backgroundColor: 'rgba(75,192,192,0.4)',
+                    borderColor: 'rgba(75,192,192,1)',
+                    pointBorderColor: 'rgba(75,192,192,1)',
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 5,
+                    pointHoverRadius: 10,
+                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                    pointHoverBorderColor: 'rgba(220,220,220,1)',
+                    pointHoverBorderWidth: 5,
+                    pointRadius: 5,
+                    pointHitRadius: 10,
+                    showLine: true,
+                    data: [...views]
+                },
+
+                {
+                    label: 'Likes',
+                    fill: false,
+                    lineTension: 1,
+                    backgroundColor: 'rgba(75,70,190,0.4)',
+                    borderColor: 'rgba(71,100,140,1)',
+                    pointBorderColor: 'rgba(75,192,192,1)',
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 5,
+                    pointHoverRadius: 10,
+                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                    pointHoverBorderColor: 'rgba(220,220,220,1)',
+                    pointHoverBorderWidth: 5,
+                    pointRadius: 5,
+                    pointHitRadius: 10,
+                    showLine: true,
+                    data: [...likes]
+                },
+
+                {
+                    label: 'Downloads',
+                    fill: false,
+                    lineTension: 1,
+                    backgroundColor: 'rgba(75,70,190,0.4)',
+                    borderColor: 'rgba(71,100,140,1)',
+                    pointBorderColor: 'rgba(75,192,192,1)',
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 5,
+                    pointHoverRadius: 10,
+                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                    pointHoverBorderColor: 'rgba(220,220,220,1)',
+                    pointHoverBorderWidth: 5,
+                    pointRadius: 5,
+                    pointHitRadius: 10,
+                    showLine: true,
+                    data: [...downloads]
+                }
+
+            ]
+        };
+
+        const pieChartData = {
+            labels: [
+                'Views',
+                'Likes',
+                'Downloads'
+            ],
+            datasets: [{
+                data: [
+                    photoStatistics.downloads && photoStatistics.downloads.total,
+                    photoStatistics.likes && photoStatistics.likes.total,
+                    photoStatistics.downloads && photoStatistics.downloads.total
+                ],
+                backgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#FFCE56'
+                ],
+                hoverBackgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#FFCE56'
+                ]
+            }]
+        };
+
         return (
             <div>
                 <h1>Photo Statistics</h1>
+
+
+                <div className="chart-container">
+
+                    <Line
+                        datasetKeyProvider={this.datasetKeyProvider}
+                        width={50}
+                        height={10}
+                        options={{
+                            legend: {
+                                display: true,
+                                position: 'bottom'
+                            },
+                            maintainAspectRatio: false,
+
+                            scales: {
+                                xAxes: [
+                                    {
+                                        type: 'time',
+                                        time: {
+                                            unit: 'day'
+                                        }
+                                    }
+                                ]
+                            }
+                        }}
+                        data={lineChartData}
+                    />
+
+                    <Pie
+                        datasetKeyProvider={this.datasetKeyProvider}
+                        data={pieChartData}
+                        width={100}
+                        height={50}
+                        options={{ maintainAspectRatio: false }}
+                    />
+                </div>
+                {this.datasetKeyProvider}
             </div>
         )
     }
@@ -38,5 +216,5 @@ export default class PhotoStatistics extends Component {
 
 
 PhotoStatistics.propTypes = {
-    photoId: PropTypes.number
+    photoId: PropTypes.string
 }
--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import Search from '../SearchBar/SearchBar';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import convertDate from '../../helpers/convertDate';
 import Carousel from '../ImageSlider/ImageSlider';
 import './Photos.css';
 import Categories from '../Categories/Categories';
@@ -50,7 +49,6 @@ export default class Photos extends Component {
 
 
     onChangeHandler = (e) => {
-
         console.log(e.target.value);
         this.setState({ userInput: e.target.value });
     }
@@ -65,7 +63,7 @@ export default class Photos extends Component {
 
 
     componentDidMount() {
-        this.fetchPhotos();
+        this.getPhotos();
         // this.getRandomPhoto(3);
     }
 
@@ -89,7 +87,7 @@ export default class Photos extends Component {
         }
     }
 
-    fetchPhotos = () => {
+    getPhotos = () => {
         const {
             count,
             start,
@@ -128,13 +126,6 @@ export default class Photos extends Component {
         }
     }
 
-    displayPhotos = () => {
-
-
-
-    }
-
-
     render() {
         const { photos, searchResult, hasQuery, userInput, randomPhotos } = this.state;
 
@@ -155,17 +146,17 @@ export default class Photos extends Component {
                         <h1>Showing photos for search term:{userInput}</h1>
                         <InfiniteScroll
                             dataLength={searchResult.length}
-                            next={this.fetchPhotos}
+                            next={this.getPhotos}
                             hasMore={true}
                             endMessage={<p style={{ textAlign: 'center' }}>
                                 <b>There is no more photo!</b>
                             </p>}>
 
                             <Masonry
-                                className={'gallery'} // default ''
-                                options={masonryOptions} // default {}
-                                disableImagesLoaded={false} // default false
-                                updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+                                className={'gallery'}
+                                options={masonryOptions}
+                                disableImagesLoaded={false}
+                                updateOnEachImageLoad={false}
                             >
                                 {searchResult.map((photo, i) => (
 
@@ -205,7 +196,7 @@ export default class Photos extends Component {
 
                         <InfiniteScroll
                             dataLength={photos.length}
-                            next={this.fetchPhotos}
+                            next={this.getPhotos}
                             hasMore={true}
                             endMessage={<p style={{ textAlign: 'center' }}>
                                 <b>There is no more photo!</b>
@@ -213,10 +204,10 @@ export default class Photos extends Component {
                             <h1 className="title">Photos</h1>
 
                             <Masonry
-                                className={'gallery'} // default ''
-                                options={masonryOptions} // default {}
-                                disableImagesLoaded={false} // default false
-                                updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+                                className={'gallery'}
+                                options={masonryOptions}
+                                disableImagesLoaded={false}
+                                updateOnEachImageLoad={false}
                             >
                                 {photos.map((photo, i) => (
 

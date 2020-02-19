8485b279b79aad0b08c23ae2f20a94d9feede10a
Umut <bozdag321@hotmail.com>
2020-02-20 00:04:43 +0300

    Add UserPhotos

--- src/components/UserPhotos/UserPhotos.js
+++ src/components/UserPhotos/UserPhotos.js
@@ -5,57 +5,56 @@ import Masonry from "react-masonry-component";
 import ModalImage from "react-modal-image";
 import { Link } from "react-router-dom";
 import { masonryOptions } from "../../helpers/masonryOptions";
-import PropTypes from "prop-types";
+import Spinner from "../Spinner/Spinner";
+import "./UserPhotos.css";
+import NoContent from "../NoContent/NoContent";
+
 export default function UserPhotos(props) {
   return (
     <div className="user-photos">
       <div>
-        <InfiniteScroll
-          dataLength={props.userPhotos.length}
-          next={props.getUserPhotos}
-          hasMore={true}
-          endMessage={
-            <p style={{ textAlign: "center" }}>
-              <b>There is no more photo!</b>
-            </p>
-          }
-        >
-          <Masonry
-            className="gallery"
-            options={masonryOptions}
-            disableImagesLoaded={false}
-            updateOnEachImageLoad={false}
+        {props.userPhotos.length != 0 ? (
+          <InfiniteScroll
+            dataLength={props.userPhotos.length}
+            next={props.getUserPhotos}
+            hasMore={true}
+            endMessage={
+              <p style={{ textAlign: "center" }}>
+                <b>There is no more photo!</b>
+              </p>
+            }
           >
-            {props.userPhotos.map((photo, i) => (
-              <div key={i}>
-                <ModalImage
-                  small={photo.urls && photo.urls.small}
-                  large={photo.urls && photo.urls.full}
-                  alt={photo.alt_description}
-                  className="photo-modal"
-                  showRotate
-                />
-                <a target="_blank" href={photo.links.download}>
-                  <Icon className="icon-download" type="download" />
-                </a>
+            <Masonry
+              className="gallery"
+              options={masonryOptions}
+              disableImagesLoaded={false}
+              updateOnEachImageLoad={false}
+            >
+              {props.userPhotos.map((photo, i) => (
+                <div key={i}>
+                  <ModalImage
+                    small={photo.urls && photo.urls.small}
+                    large={photo.urls && photo.urls.full}
+                    alt={photo.alt_description}
+                    className="photo-modal"
+                    showRotate
+                  />
+                  <a target="_blank" href={photo.links.download}>
+                    <Icon className="icon-download" type="download" />
+                  </a>
 
-                <div className="photo-info">
-                  <Link to={`/users/${photo.user.username}`}>
-                    <Avatar
-                      className="user-avatar"
-                      src={photo.user.profile_image.large}
-                    />
-                    <p className="username">{photo.user.name}</p>
-                  </Link>
-
-                  <Link to={`/photos/${photo.id}`}>
-                    <Icon className="icon-circle" type="info-circle" />
-                  </Link>
+                  <div className="photo-info">
+                    <Link to={`/photos/${photo.id}`}>
+                      <Icon className="icon-circle" type="info-circle" />
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </Masonry>
-        </InfiniteScroll>
+              ))}
+            </Masonry>
+          </InfiniteScroll>
+        ) : (
+          <NoContent />
+        )}
       </div>
     </div>
   );

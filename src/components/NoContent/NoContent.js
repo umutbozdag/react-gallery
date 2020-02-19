320f7ce6252dda8ee6dfbd31e37af1de1c7eb8c6
Umut <bozdag321@hotmail.com>
2020-02-19 23:55:33 +0300

    Add NoContent Component

---
+++ src/components/NoContent/NoContent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./NoContent.css";
+
+export default function NoContent() {
+  return (
+    <div>
+      <p className="no-content">We couldn&#39;t find anything :(</p>
+    </div>
+  );
+}

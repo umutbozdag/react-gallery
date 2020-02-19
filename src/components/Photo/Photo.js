678443a8265fc703275683d403cbf95c934d7bd2
Umut <bozdag321@hotmail.com>
2020-02-19 23:59:44 +0300

    Add the Spinner

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -6,10 +6,11 @@ import ModalImage from "react-modal-image";
 import { Link } from "react-router-dom";
 import { masonryOptions } from "../../helpers/masonryOptions";
 import PropTypes from "prop-types";
+import Spinner from "../Spinner/Spinner";
 
 export default function Photo(props) {
   return (
-    <div>
+    <div className="container">
       {props.hasQuery ? (
         props.searchResult.length != 0 ? (
           <div>
@@ -61,56 +62,62 @@ export default function Photo(props) {
             </InfiniteScroll>
           </div>
         ) : (
-          <p className="no-results">No results :(</p>
+          <Spinner />
         )
       ) : (
-        <div className="container">
-          <InfiniteScroll
-            dataLength={props.photos.length}
-            next={props.getPhotos}
-            hasMore={true}
-            endMessage={
-              <p style={{ textAlign: "center" }}>
-                <b>There is no more photo!</b>
-              </p>
-            }
-          >
-            <Masonry
-              className="gallery"
-              options={masonryOptions}
-              disableImagesLoaded={false}
-              updateOnEachImageLoad={false}
-            >
-              {props.photos.map((photo, i) => (
-                <div key={i}>
-                  <ModalImage
-                    small={photo.urls && photo.urls.small}
-                    large={photo.urls && photo.urls.full}
-                    alt={photo.alt_description}
-                    className="photo-modal"
-                    showRotate
-                  />
-                  <a target="_blank" href={photo.links.download}>
-                    <Icon className="icon-download" type="download" />
-                  </a>
-
-                  <div className="photo-info">
-                    <Link to={`/users/${photo.user.username}`}>
-                      <Avatar
-                        className="user-avatar"
-                        src={photo.user.profile_image.large}
+        <div>
+          {props.photos.length != 0 ? (
+            <div className="container">
+              <InfiniteScroll
+                dataLength={props.photos.length}
+                next={props.getPhotos}
+                hasMore={true}
+                endMessage={
+                  <p style={{ textAlign: "center" }}>
+                    <b>There is no more photo!</b>
+                  </p>
+                }
+              >
+                <Masonry
+                  className="gallery"
+                  options={masonryOptions}
+                  disableImagesLoaded={false}
+                  updateOnEachImageLoad={false}
+                >
+                  {props.photos.map((photo, i) => (
+                    <div key={i}>
+                      <ModalImage
+                        small={photo.urls && photo.urls.small}
+                        large={photo.urls && photo.urls.full}
+                        alt={photo.alt_description}
+                        className="photo-modal"
+                        showRotate
                       />
-                      <p className="username">{photo.user.name}</p>
-                    </Link>
+                      <a target="_blank" href={photo.links.download}>
+                        <Icon className="icon-download" type="download" />
+                      </a>
 
-                    <Link to={`/photos/${photo.id}`}>
-                      <Icon className="icon-circle" type="info-circle" />
-                    </Link>
-                  </div>
-                </div>
-              ))}
-            </Masonry>
-          </InfiniteScroll>
+                      <div className="photo-info">
+                        <Link to={`/users/${photo.user.username}`}>
+                          <Avatar
+                            className="user-avatar"
+                            src={photo.user.profile_image.large}
+                          />
+                          <p className="username">{photo.user.name}</p>
+                        </Link>
+
+                        <Link to={`/photos/${photo.id}`}>
+                          <Icon className="icon-circle" type="info-circle" />
+                        </Link>
+                      </div>
+                    </div>
+                  ))}
+                </Masonry>
+              </InfiniteScroll>
+            </div>
+          ) : (
+            <Spinner />
+          )}
         </div>
       )}
     </div>

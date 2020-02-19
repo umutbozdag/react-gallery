131030732c2219ec7df5352e4dab887a0300cb68
Umut <bozdag321@hotmail.com>
2020-02-20 00:04:31 +0300

    Add UserCollection

--- src/components/UserCollections/UserCollections.js
+++ src/components/UserCollections/UserCollections.js
@@ -1,9 +1,71 @@
 import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Link } from "react-router-dom";
+import { Spin, Tag } from "antd";
+import Masonry from "react-masonry-component";
+import { masonryOptions } from "../../helpers/masonryOptions";
+import NoContent from "../NoContent/NoContent";
 
 export default function UserCollections(props) {
   return (
     <div>
-      <h1>User Collections</h1>
+      {props.userCollections.cover_photo ? (
+        <div>
+          {props.userCollections.length != 0 ? (
+            <div className="collections">
+              <InfiniteScroll
+                dataLength={props.userCollections.length}
+                next={props.getUserCollections}
+                hasMore={true}
+                loader={<Spin />}
+                endMessage={<NoContent />}
+              >
+                <Masonry
+                  className="gallery"
+                  options={masonryOptions}
+                  disableImagesLoaded={false}
+                  updateOnEachImageLoad={false}
+                >
+                  {props.userCollections.map((photo, i) => (
+                    <div className="collection" key={i}>
+                      <Link to={`/collections/${photo.id}`}>
+                        <div className="collection-photo">
+                          <img
+                            className="collection-photo"
+                            src={
+                              photo.cover_photo.urls &&
+                              photo.cover_photo.urls.small
+                            }
+                            alt=""
+                          />
+                        </div>
+                      </Link>
+                      <Link to={`/collections/${photo.id}`}>
+                        <p className="photo-title">{photo.title}</p>
+                      </Link>
+                      <p className="total-photos">
+                        {photo.total_photos} Photos
+                      </p>
+                      <div className="collection-tags">
+                        {photo.tags.slice(0, 3).map((tag, index) => (
+                          <Tag color="#161616" className="tag" key={index}>
+                            {tag.title}
+                          </Tag>
+                        ))}
+                      </div>
+                    </div>
+                  ))}
+                </Masonry>
+              </InfiniteScroll>
+            </div>
+          ) : (
+            <NoContent />
+          )}
+          &nbsp;
+        </div>
+      ) : (
+        <NoContent />
+      )}
     </div>
   );
 }
